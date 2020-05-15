7714fe1f8b6780c8bccd0fe66f3e2ad4c38d0eb0
David MacCormack <35852337+dmaccormack@users.noreply.github.com>
2020-05-15 02:15:55 -0400

    Chore: Add example2
    The existing example POM was refactored and a new parent POM was
    created from the original. A second example was added which uses
    the new parent POM.

---
+++ example/example2/src/main/java/org/jsonurl/example2/Main.java
@@ -0,0 +1,74 @@
+package org.jsonurl.example2;
+
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsonurl.JsonUrlStringBuilder;
+import org.jsonurl.jsonorg.JsonUrlParser;
+import org.jsonurl.jsonorg.JsonUrlWriter;
+
+public class Main {
+    
+    /**
+     * Return an example value.
+     */
+    public static final JSONArray getExampleValue() {
+        return new JSONArray()
+                .put(new JSONObject()
+                    .put("id", 1)
+                    .put("name", new JSONObject()
+                            .put("firstName", "Montgomery")
+                            .put("lastName", "Burns")
+                            .put("evil", true)
+                            .put("age", JSONObject.NULL)))
+                .put(new JSONObject()
+                        .put("id", 2)
+                        .put("name", new JSONObject()
+                                .put("firstName", "Homer")
+                                .put("lastName", "Simpson")
+                                .put("evil", false)
+                                .put("age", 36)
+                                .put("oddKeyName=", false)
+                                .put("oddKeyName&", true)
+                                .put("101.5", 101.5)
+                                .put("1e1", 1e1)
+                                .put("15e+1", 15e+1)
+                                .put("16e-1", 16e-1)
+                                .put("\"text\"", "funny+key")));
+    }
+
+    /**
+     * Serialize the given JsonArray and return a String.
+     */
+    public static final String getExampleValueText(JSONArray value) throws IOException {
+        JsonUrlStringBuilder text = new JsonUrlStringBuilder();
+        JsonUrlWriter.write(text, value);
+        return text.build();
+    }
+
+    /**
+     * OS entry point.
+     */
+    public static final void main(String[] args) throws IOException {
+
+        //
+        // get my example value as a POJO
+        //
+        JSONArray value = getExampleValue();
+        
+        //
+        // turn that into text
+        //
+        String jsonUrlText = getExampleValueText(value);
+
+        System.out.println(jsonUrlText);
+
+        //
+        // parse the text and build a JsonArray
+        //
+        JsonUrlParser p = new JsonUrlParser();
+        JSONArray parsedValue = p.parseArray(jsonUrlText);
+        //parsedValue.write(new PrintWriter(System.out));
+        System.out.println(parsedValue.toString());
+    }
+}
---
+++ example/example2/src/test/java/org/jsonurl/example2/Example2Test.java
@@ -0,0 +1,42 @@
+package org.jsonurl.example2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import org.json.JSONArray;
+import org.jsonurl.JsonUrlStringBuilder;
+import org.jsonurl.example2.Main;
+import org.jsonurl.jsonorg.JsonUrlParser;
+import org.jsonurl.jsonorg.JsonUrlWriter;
+import org.junit.jupiter.api.Test;
+
+class Example2Test {
+
+    @Test
+    void test() throws IOException {
+        //
+        // get my example value as a POJO
+        //
+        JSONArray value = Main.getExampleValue();
+
+        //
+        // turn that into JSON-&gt;URL text
+        //
+        JsonUrlStringBuilder text = new JsonUrlStringBuilder();
+        JsonUrlWriter.write(text, value);
+        String jsonUrlText = text.build();
+
+        //
+        // parse the JSON-&gt;URL text and build a JsonArray
+        //
+        JsonUrlParser p = new JsonUrlParser();
+        JSONArray parsedValue = p.parseArray(jsonUrlText);
+        
+        //
+        // I'm testing the string values because JSONArray does not override
+        // the equals() method so the test fails even though the values are
+        // semantically equivalent.
+        //
+        assertEquals(value.toString(), parsedValue.toString());
+    }
+}
