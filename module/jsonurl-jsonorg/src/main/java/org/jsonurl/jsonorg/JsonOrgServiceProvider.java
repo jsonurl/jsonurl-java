acb3115c5c7c8c6fd1f14e63ee62ca540a8b6e05
David MacCormack <35852337+dmaccormack@users.noreply.github.com>
2020-07-11 00:40:27 -0400

    doc: remove extraneous comment whitespace
    PMD found extraneous comment whitespace; it has been removed. Also,
    an additional reference in the ruleset was added. This was caused by
    an Eclipse PMD plugin version bump. The rule is commented
    out as it's not compatible with the version packaged with Maven.

--- module/jsonurl-jsonorg/src/main/java/org/jsonurl/jsonorg/JsonOrgServiceProvider.java
+++ module/jsonurl-jsonorg/src/main/java/org/jsonurl/jsonorg/JsonOrgServiceProvider.java
@@ -2,7 +2,7 @@ package org.jsonurl.jsonorg;
 
 /*
  * Copyright 2019 David MacCormack
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
  * of the License at
