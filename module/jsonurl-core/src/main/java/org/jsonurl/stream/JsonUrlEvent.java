fd081240f5286a03f0ca1da63085cfa0d0bf66dd
David MacCormack <35852337+dmaccormack@users.noreply.github.com>
2021-05-11 21:37:16 -0400

    doc: Add references to the spec for relevant options

--- module/jsonurl-core/src/main/java/org/jsonurl/stream/JsonUrlEvent.java
+++ module/jsonurl-core/src/main/java/org/jsonurl/stream/JsonUrlEvent.java
@@ -27,46 +27,70 @@ package org.jsonurl.stream;
 public enum JsonUrlEvent {
     /**
      * Start of a JSON&#x2192;URL object.
+     * @see <a href="https://github.com/jsonurl/specification#23-objects"
+     * >JSON&#x2192;URL specification, section 2.3</a>
      */
     START_OBJECT,
     /**
      * End of a JSON&#x2192;URL object.
+     * @see <a href="https://github.com/jsonurl/specification#23-objects"
+     * >JSON&#x2192;URL specification, section 2.3</a>
      */
     END_OBJECT,
     /**
      * Start of a JSON&#x2192;URL array.
+     * @see <a href="https://github.com/jsonurl/specification#24-arrays"
+     * >JSON&#x2192;URL specification, section 2.4</a>
      */
     START_ARRAY,
     /**
      * End of a JSON&#x2192;URL array.
+     * @see <a href="https://github.com/jsonurl/specification#24-arrays"
+     * >JSON&#x2192;URL specification, section 2.4</a>
      */
     END_ARRAY,
     /**
      * A JSON&#x2192;URL object key name.
+     * @see <a href="https://github.com/jsonurl/specification#23-objects"
+     * >JSON&#x2192;URL specification, section 2.3</a>
      */
     KEY_NAME,
     /**
      * A literal {@code false}.
+     * @see <a href="https://github.com/jsonurl/specification#21-values"
+     * >JSON&#x2192;URL specification, section 2.1</a>
      */
     VALUE_FALSE,
     /**
      * A literal {@code true}.
+     * @see <a href="https://github.com/jsonurl/specification#21-values"
+     * >JSON&#x2192;URL specification, section 2.1</a>
      */
     VALUE_TRUE,
     /**
      * A literal {@code null}.
+     * @see <a href="https://github.com/jsonurl/specification#21-values"
+     * >JSON&#x2192;URL specification, section 2.1</a>
      */
     VALUE_NULL,
     /**
      * A number literal.
+     * @see <a href="https://github.com/jsonurl/specification#26-numbers"
+     * >JSON&#x2192;URL specification, section 2.6</a>
      */
     VALUE_NUMBER,
     /**
      * A string literal.
+     * @see <a href="https://github.com/jsonurl/specification#25-strings"
+     * >JSON&#x2192;URL specification, section 2.5</a>
+     * @see <a href="https://github.com/jsonurl/specification#296-address-bar-query-string-friendly"
+     * >JSON&#x2192;URL specification, section 2.9.6</a>
      */
     VALUE_STRING,
     /**
      * The empty composite.
+     * @see <a href="https://github.com/jsonurl/specification#22-composites"
+     * >JSON&#x2192;URL specification, section 2.2</a>
      */
     VALUE_EMPTY_COMPOSITE,
     /**
@@ -76,6 +100,8 @@ public enum JsonUrlEvent {
     /**
      * A missing value (e.g. object key name with no
      * following separator and value).
+     * @see <a href="https://github.com/jsonurl/specification#294-implied-object-missing-values"
+     * >JSON&#x2192;URL specification, section 2.9.4</a>
      */
     VALUE_MISSING,
     /**
